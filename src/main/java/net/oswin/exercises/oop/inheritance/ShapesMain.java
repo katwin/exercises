903aa57ab3ef5ae78e4d0b4c02413386cb43bf55
katwin <efekma@gmail.com>
2017-04-28 19:09:11 +0300

    Проверяла исправность работы методов классов фигур, исправила недочеты и ошибки в классе Triangle

--- src/main/java/net/oswin/exercises/oop/inheritance/ShapesMain.java
+++ src/main/java/net/oswin/exercises/oop/inheritance/ShapesMain.java
@@ -1,10 +1,29 @@
 package net.oswin.exercises.oop.inheritance;
 
 /**
- * Created by Kate on 27.04.2017.
+ * Работа с фигурами.
  */
 public class ShapesMain {
     public static void main(String[] args) {
-        Shape shape = new Shape();
+        Triangle triangle = new Triangle(3, 4, 90);
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(5, 7);
+        Square square = new Square(6);
+
+        System.out.println("Треугольник:");
+        System.out.println("S = " + triangle.getSquare());
+        System.out.println("P = " + triangle.getPerimeter());
+
+        System.out.println("Круг:");
+        System.out.println("S = " + circle.getSquare());
+        System.out.println("l = " + circle.getPerimeter());
+
+        System.out.println("Прямоугольник:");
+        System.out.println("S = " + rectangle.getSquare());
+        System.out.println("P = " + rectangle.getPerimeter());
+
+        System.out.println("Квадрат:");
+        System.out.println("S = " + square.getSquare());
+        System.out.println("P = " + square.getPerimeter());
     }
 }
--- src/main/java/net/oswin/exercises/oop/inheritance/Triangle.java
+++ src/main/java/net/oswin/exercises/oop/inheritance/Triangle.java
@@ -13,4 +13,14 @@ public class Triangle extends Shape {
         this.side2 = side2;
         this.angle = angle;
     }
+
+    @Override
+    public double getSquare() {
+        return side1 * side2 * Math.sin(angle * Math.PI / 180) / 2;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side1 + side2 + Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2) - 2 * side1 * side2 * Math.cos(angle * Math.PI / 180));
+    }
 }
