3a5ff27c59c4c93171dca31554deeac6dc71a1c6
katwin <efekma@gmail.com>
2017-06-16 21:44:52 +0300

    Подправила программу счетчика слов в тексте.

--- src/main/java/net/oswin/exercises/WordsCounter.java
+++ src/main/java/net/oswin/exercises/WordsCounter.java
@@ -10,7 +10,7 @@ public class WordsCounter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите текст:");
-        String[] text = sc.nextLine().replaceAll("[^A-Za-zА-Яа-я0-9]", " ").replaceAll("\\s+", " ").split(" ");
-        System.out.println("Количество слов в тексте - " + text.length);
+        String[] text = sc.nextLine().replaceAll("\\s+", " ").split(" ");
+        System.out.println("Количество слов в тексте: " + text.length);
     }
 }
